ad7a341cb941ec416ff6f6c43e0924b4469d450d
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2016-12-29 14:31:24 -0200

    Basic menu written

---
+++ entry.go
@@ -0,0 +1,10 @@
+package moneylog
+
+type Entry struct {
+    Description string
+    Value float64
+}
+
+func NewEntry(description string, value float64) *Entry {
+    return &Entry{Description: description, Value: value}
+}
--- prompt/prompt.go
+++ prompt/prompt.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
+    var running bool = true
+    var option int = 0
+
+    for running {
+        menu()
+        fmt.Scanf("%d\n", &option)
+
+        if option == 0 {
+            running = false
+        }
+    }
+}
+
+func menu() {
     fmt.Println("# MONEY LOGS")
+    fmt.Println("0. EXIT")
+    fmt.Println("1. ADD ENTRY")
+    fmt.Println("2. SHOW ENTRIES")
+    fmt.Println("Choose an option:")
 }
