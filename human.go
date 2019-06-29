77809b284edc0d8c71483a669d7071f1c460ed80
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-06-29 13:53:57 -0300

    Discern monetary value from expense description

--- human.go
+++ human.go
@@ -1,10 +1,39 @@
 package logey
 
 import (
+	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 )
 
-func Understand(human string) Entry {
-	outlet := NewEntry("Besoldung", 1000, make([]string, 0), time.Now())
-	return outlet
+func Understand(human string) (Entry, error) {
+	outlet := NewEntry("", 0, make([]string, 0), time.Now())
+
+	moneyRegex, oops := regexp.Compile("(-?[$€円¥][0-9]*)")
+	if oops != nil {
+		return outlet, oops
+	}
+	currencyRegex, oops := regexp.Compile("[$€円¥]")
+	if oops != nil {
+		return outlet, oops
+	}
+
+	rawMoney := moneyRegex.Find([]byte(human))
+	if rawMoney == nil {
+		return outlet, errors.New("Unable to find monetary value in description")
+	}
+	valueDescription := currencyRegex.ReplaceAll(rawMoney, []byte(""))
+	howMuch, oops := strconv.ParseFloat(string(valueDescription), 64)
+	if oops != nil {
+		return outlet, oops
+	}
+
+	how := strings.ReplaceAll(human, string(rawMoney), "")
+
+	outlet.How = strings.TrimSpace(how)
+	outlet.HowMuch = howMuch
+
+	return outlet, nil
 }
--- human_test.go
+++ human_test.go
@@ -8,8 +8,29 @@ import (
 func TestLogeyCanConvertSimpleStringIntoEntries(t *testing.T) {
     testCase := "Besoldung $1000"
     expectedEntry := NewEntry("Besoldung", 1000, make([]string, 0), time.Now())
-    resultEntry := Understand(testCase)
+    resultEntry, oops := Understand(testCase)
+    if oops != nil {
+        t.Error("Failed valid test case")
+    }
+    if expectedEntry.How != resultEntry.How {
+        t.Error("Descriptions don't match")
+    }
+    if expectedEntry.HowMuch != resultEntry.HowMuch {
+        t.Error("Values don't match")
+    }
 
+    testCase = "This test case contains no money description"
+    _, oops = Understand(testCase)
+    if oops == nil {
+        t.Error("Succeeded invalid test case")
+    }
+
+    testCase = "-€500 Steuer"
+    expectedEntry = NewEntry("Steuer", -500, make([]string, 0), time.Now())
+    resultEntry, oops = Understand(testCase)
+    if oops != nil {
+        t.Error("Failed valid test case")
+    }
     if expectedEntry.How != resultEntry.How {
         t.Error("Descriptions don't match")
     }
