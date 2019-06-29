35d5d50653fb0119cabd8c0dc2d9239e6778ac29
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-06-29 19:26:11 -0300

    Load dates from human strings

--- human.go
+++ human.go
@@ -8,19 +8,24 @@ import (
 	"time"
 )
 
+const (
+	MONEY_REGEX    = "(-?[$€円¥][0-9]*)"
+	CURRENCY_REGEX = "[$€円¥]"
+	DATE_REGEX     = "([0-9]{4}-0?[0-9]{1,2}-0?[0-9]{1,2})"
+)
+
 func Understand(human string) (Entry, error) {
+	inlet := []byte(human)
 	outlet := NewEntry("", 0, make([]string, 0), time.Now())
 
-	moneyRegex, oops := regexp.Compile("(-?[$€円¥][0-9]*)")
-	if oops != nil {
-		return outlet, oops
-	}
-	currencyRegex, oops := regexp.Compile("[$€円¥]")
+	moneyRegex, _ := regexp.Compile(MONEY_REGEX)
+	currencyRegex, _ := regexp.Compile(CURRENCY_REGEX)
+	dateRegex, oops := regexp.Compile(DATE_REGEX)
 	if oops != nil {
 		return outlet, oops
 	}
 
-	rawMoney := moneyRegex.Find([]byte(human))
+	rawMoney := moneyRegex.Find(inlet)
 	if rawMoney == nil {
 		return outlet, errors.New("Unable to find monetary value in description")
 	}
@@ -30,10 +35,22 @@ func Understand(human string) (Entry, error) {
 		return outlet, oops
 	}
 
+	rawDate := string(dateRegex.Find(inlet))
+	when := time.Now()
+	if len(rawDate) > 0 {
+		pieces := strings.Split(rawDate, "-")
+		year, _ := strconv.Atoi(pieces[0])
+		month, _ := strconv.Atoi(pieces[1])
+		day, _ := strconv.Atoi(pieces[2])
+		when = time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
+	}
+
 	how := strings.ReplaceAll(human, string(rawMoney), "")
+	how = strings.ReplaceAll(how, rawDate, "")
 
 	outlet.How = strings.TrimSpace(how)
 	outlet.HowMuch = howMuch
+	outlet.When = when
 
 	return outlet, nil
 }
--- human_test.go
+++ human_test.go
@@ -38,3 +38,29 @@ func TestLogeyCanConvertSimpleStringIntoEntries(t *testing.T) {
         t.Error("Values don't match")
     }
 }
+
+func TestLogeyCanLoadDates(t *testing.T) {
+    testCase := "電子レンジ 円500 2019-01-01"
+    expecteDate := time.Date(2019, time.January, 1, 0, 0, 0, 0, time.UTC)
+    expectedEntry := NewEntry("電子レンジ", 500, make([]string, 0), expecteDate)
+    resultEntry, oops := Understand(testCase)
+    if oops != nil {
+        t.Error("Failed valid test case")
+    }
+    if expectedEntry.How != resultEntry.How {
+        t.Error("Descriptions don't match")
+    }
+    if expectedEntry.HowMuch != resultEntry.HowMuch {
+        t.Error("Values don't match")
+    }
+    resultDate := resultEntry.When
+    if expecteDate.Year() != resultDate.Year() {
+        t.Error("Loaded wrong year")
+    }
+    if expecteDate.Month() != resultDate.Month() {
+        t.Error("Loaded wrong month")
+    }
+    if expecteDate.Day() != resultDate.Day() {
+        t.Error("Loaded wrong day")
+    }
+}
